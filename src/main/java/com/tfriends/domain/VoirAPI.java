495211a68bdeb8135f864e04ace6e918b547b6f8
LRoseline <amb17@naver.com>
2022-05-06 15:36:37 +0900

    Date added

--- src/main/java/com/tfriends/domain/VoirAPI.java
+++ src/main/java/com/tfriends/domain/VoirAPI.java
@@ -4,8 +4,11 @@ import lombok.Data;
 
 @Data
 public class VoirAPI {
+    private String Tdate;
     private String Twlevel;
     private String Trate;
+
+    private String Ydate;
     private String Ywlevel;
     private String Yrate;
 }
