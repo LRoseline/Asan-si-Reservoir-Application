60a0feb4f8db767e48eea2ac2e3282eef48c5892
LRoseline <amb17@naver.com>
2022-05-11 17:33:01 +0900

    추가수정 예정

--- src/main/java/com/tfriends/domain/VoirAPI.java
+++ src/main/java/com/tfriends/domain/VoirAPI.java
@@ -13,4 +13,32 @@ public class VoirAPI {
     private String Ydate;
     private String Ywlevel;
     private String Yrate;
+
+    // Will be Update
+    private String date8;
+    private String date7;
+    private String date6;
+    private String date5;
+    private String date4;
+    private String date3;
+    private String date2;
+    private String date1;
+
+    private String wlevel8;
+    private String wlevel7;
+    private String wlevel6;
+    private String wlevel5;
+    private String wlevel4;
+    private String wlevel3;
+    private String wlevel2;
+    private String wlevel1;
+
+    private String rate8;
+    private String rate7;
+    private String rate6;
+    private String rate5;
+    private String rate4;
+    private String rate3;
+    private String rate2;
+    private String rate1;
 }
