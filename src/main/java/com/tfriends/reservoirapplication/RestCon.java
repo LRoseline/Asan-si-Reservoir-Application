84c39879b41433d6d3fe9c8ef40fbcd12a622b6b
LRoseline <amb17@naver.com>
2022-12-16 15:38:36 +0900

    jsdelivr

--- src/main/java/com/tfriends/reservoirapplication/RestCon.java
+++ src/main/java/com/tfriends/reservoirapplication/RestCon.java
@@ -78,53 +78,33 @@ public class RestCon {
 
 		String [] datesdf = {sdf.format(dates[0].getTime()), sdf.format(dates[1].getTime())};
 
-		String xml;
-		String URLComp = "http://apis.data.go.kr/B552149/reserviorWaterLevel/reservoirlevel/?serviceKey="+setting.SettingLoad("datagokr").getValue()+"&pageNo=1&numOfRows=10&fac_code="+api.getCode()+"&date_s="+datesdf[0]+"&date_e="+datesdf[1];
+		String URLComp = "http://apis.data.go.kr/B552149/reserviorWaterLevel/reservoirlevel/?serviceKey="+setting.SettingLoad("datagokr").getValue()+"&pageNo=1&numOfRows="+10+"&fac_code="+api.getCode()+"&date_s="+datesdf[0]+"&date_e="+datesdf[1];
 
-        URL url = new URL(URLComp);
-        HttpURLConnection http = (HttpURLConnection)url.openConnection();
-//        http.setConnectTimeout(10000);
-        http.setUseCaches(false);
+        DocumentBuilderFactory factory = DocumentBuilderFactory
+                .newInstance();
+        DocumentBuilder documentBuilder = factory.newDocumentBuilder();
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(http.getInputStream()));
-        StringBuilder sb = new StringBuilder();
-        while (true) {
-            String line = br.readLine();
-            if (line == null)
-                break;
-            sb.append(line);
+        Document doc = documentBuilder.parse(URLComp);
+        Element element = doc.getDocumentElement();
+
+        NodeList [] voir = {
+            element.getElementsByTagName("fac_name"),
+            element.getElementsByTagName("check_date"),
+            element.getElementsByTagName("rate"),
+            element.getElementsByTagName("water_level")
+        };
+
+        List<VoirDaily> appmedia = new ArrayList<VoirDaily>();
+        for (int i = 0; i < 8; i++) {
+            VoirDaily days = new VoirDaily();
+            
+            days.setDate(voir[1].item(i).getFirstChild().getNodeValue());
+            days.setRate(voir[2].item(i).getFirstChild().getNodeValue());
+            days.setWlevel(voir[3].item(i).getFirstChild().getNodeValue());
+            appmedia.add(i, days);
+            api.setDaily(appmedia);
         }
-        xml = sb.toString();
-        br.close();
-        http.disconnect();
 
-        if (xml != null) {
-	        DocumentBuilderFactory factory = DocumentBuilderFactory
-	                .newInstance();
-	        DocumentBuilder documentBuilder = factory.newDocumentBuilder();
-	
-	        InputStream is =new ByteArrayInputStream(xml.getBytes());
-	        Document doc = documentBuilder.parse(is);
-	        Element element = doc.getDocumentElement();
-
-            NodeList [] voir = {
-                element.getElementsByTagName("fac_name"),
-                element.getElementsByTagName("check_date"),
-                element.getElementsByTagName("rate"),
-                element.getElementsByTagName("water_level")
-            };
-
-            List<VoirDaily> appmedia = new ArrayList<VoirDaily>();
-            for (int i = 0; i < 8; i++) {
-                VoirDaily days = new VoirDaily();
-                
-                days.setDate(voir[1].item(i).getFirstChild().getNodeValue());
-                days.setRate(voir[2].item(i).getFirstChild().getNodeValue());
-                days.setWlevel(voir[3].item(i).getFirstChild().getNodeValue());
-                appmedia.add(i, days);
-                api.setDaily(appmedia);
-            }
-        }
         return api;
     }
 
