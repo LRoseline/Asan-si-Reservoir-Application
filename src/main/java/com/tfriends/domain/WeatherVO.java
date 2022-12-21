88325949690802226f6011a131d1f290f6f434fc
LRoseline <amb17@naver.com>
2022-12-21 15:49:56 +0900

    dropping

--- src/main/java/com/tfriends/domain/WeatherVO.java
+++ src/main/java/com/tfriends/domain/WeatherVO.java
@@ -27,22 +27,27 @@ public class WeatherVO extends DustStationVO{
 	private double drop;
 	
 	private String weather1;
+	private double drop1;
 	private Long temp1;
 	private Long time1;
 
 	private String weather2;
+	private double drop2;
 	private Long temp2;
 	private Long time2;
 
 	private String weather3;
+	private double drop3;
 	private Long temp3;
 	private Long time3;
 
 	private String weather4;
+	private double drop4;
 	private Long temp4;
 	private Long time4;
 
 	private String weather5;
+	private double drop5;
 	private Long temp5;
 	private Long time5;
 }
